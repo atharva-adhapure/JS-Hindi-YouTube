722a43fd82efc52d4793b0b3594cde6c7b9c5686
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-23 06:37:48 +0000

    conversion done

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,7 +1,7 @@
-// let score = true
+let score = true
 // console.log(typeof(score))
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 // console.log(typeof(valueInNumber))
 // console.log(valueInNumber);
 
@@ -13,7 +13,7 @@
 let isLoggedIn = ""
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 
 //"" => false
 // anything in the string returns => true
@@ -22,5 +22,31 @@ console.log(booleanisLoggedIn)
 let someNumber = 33;
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************** Operation ****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = " Atharva"
+
+let str3 = str1 + str2 
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1+ 2 + "2")
+
+// jo pehle likha jayega ussi type mai conversion hoga 
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2 
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);

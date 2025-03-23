fc296cf84a49638504f2f023b09221f9a72581da
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-23 05:58:40 +0000

    datatypes

--- 01_basics/02_data_types.js
+++ 01_basics/02_data_types.js
@@ -2,3 +2,25 @@
 
 //alert(3+3) we are using nodejs not a browser
 
+console.log(3
+    +3); // code readability should be high
+
+console.log("atharva");
+
+let name = "Atharva";
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power of 53
+//bigint
+// string => ""
+// Boolean => true/false
+//null = standalone value
+// undefined 
+// symbol => used mostly in react
+
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object

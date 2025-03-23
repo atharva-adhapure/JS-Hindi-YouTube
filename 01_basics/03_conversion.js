1e2ebb2bcb7bfd8794686bdc30b86fe428d6883a
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-23 06:10:22 +0000

    conversion of data types

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,26 @@
+// let score = true
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+
+//'33' => 33
+//33abc => NaN = Not a Number
+//true => 1/ false => 0
+
+let isLoggedIn = ""
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+//"" => false
+// anything in the string returns => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)

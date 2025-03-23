9853ca26ef9889b90b516a0c0f9e40c406131c8f
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-23 15:44:24 +0000

    strings done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "Atharva "
+const repoCount = 50
+
+// console.log(name + repoCount) //outdated syntax instead use " `  ` "
+
+// console.log(`Hello My Name is ${name} and  my repoCount is ${repoCount}`)
+
+const gameName = new String('atharva-adhapure')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf("t") )
+
+// const newString = gameName.substring(0, 4); // (Last digit is not considered)
+// // console.log(newString)
+
+// let anotherString = gameName.slice(-7,-1)
+// console.log(anotherString)
+
+// const url = "https//hitesh.com/hitesh%20Chaudhary";
+// console.log(url.replace("%20", "-"))
+
+// console.log(url.includes("hitesh"))
+
+console.log(gameName.split("-"))

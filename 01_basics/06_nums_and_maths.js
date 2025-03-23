34fa3c7eeacdeac7060f749822d39bfb121a6328
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-23 17:24:05 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,29 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+const otherNumber = 23.56467
+
+// console.log(otherNumber.toPrecision(2))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+
+
+// +++++++++++++++++++ Math +++++++++++++++++++++++
+
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.floor(4.2))
+
+console.log(Math.floor(Math.random()*10))
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1) + min))

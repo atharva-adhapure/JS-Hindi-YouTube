f4682bb74d378b176977369dc609bfc94d76dbef
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-23 06:46:53 +0000

    comparisons done

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1)
+// console.log(2>=1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// Comparison and equality check works a bit differently
+
+// === is a strict checks 
+//It checks for both the value as well as data type 
+
+console.log("2" === 2)

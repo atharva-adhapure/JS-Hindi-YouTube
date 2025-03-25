31981e2e6d12733ae41a3dac0a191b6076ededc9
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-25 18:30:22 +0000

    objects 1 done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -20,10 +20,20 @@ const JsUser = {
 // console.log(JsUser["email"]); // Standard method to access the data present in the object
 
 // // console.log(JsUser["full name"]);
-console.log(JsUser[mySym])          //Always access the data in the Object using square braces 
+// console.log(JsUser[mySym])         //Always access the data in the Object using square braces 
 
 JsUser.email = "atharva@chatgpt.com"
-Object.freeze(JsUser)               // to prevent any changes being made in the object
+// Object.freeze(JsUser)               // to prevent any changes being made in the object
 JsUser.email = "atharva@microsoft.com";
 
-console.log(JsUser.email);
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log(`Hello JS user`);
+}
+
+JsUser.greetingtwo = function(){
+    console.log(`Hello JS user, ${this.name} `);
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingtwo())

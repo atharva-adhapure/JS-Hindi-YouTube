64e554ba9920f2b8b9fbda4a4a040d503e3a5c42
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-03-28 08:27:20 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,32 @@
+function sayMyName(){
+    console.log("A")
+    console.log("T")
+    console.log("H")
+    console.log("A")
+    console.log("R")
+    console.log("V")
+    console.log("A")
+}
+
+// sayMyName()
+
+// function add( a ,b){
+//     console.log(a+b);
+// }
+
+function add(a,b){
+    let result = a+b;
+    return result
+}
+const result = add(3,4 )
+// console.log("Result:",result)
+
+function loginuserMessage(username = "Sam"){
+    if(!username){
+        console.log("Please enter the username!")
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginuserMessage("Atharva"))

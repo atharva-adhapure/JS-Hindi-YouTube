36850b96191c325bb75c10da66bbeb0075be3f5b
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-04-03 19:56:26 +0000

    if else else if

---
+++ 04_controlflow/one.js
@@ -0,0 +1,42 @@
+// if 
+// let = isUserLoggedIn = true;
+// let temperature = 41;
+
+// if(temperature == 41){
+//     console.log("The temperature is greater than 40");
+// } else{
+//     console.log("temperature below than 40");   
+// }
+// // > , < , >=, <=, == , !=, ===, !==
+
+// let score = 200 
+// if(score > 100 ){
+//     let power = "fly"               // var keyword has global scope and is not used in practice instead let and const are used
+
+//     console.log(`User has power: ${power}`);
+    
+// }
+// // console.log(`User has power: ${power}`);      error:-   power is not defined as the scope is not global
+
+
+
+// Shorthand notation
+
+// const balance = 1000;
+
+// // if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("lesss than 500");
+// }
+// else if (balance < 750) {
+//     console.log("less than 750");
+// }
+// else if (balance < 900) {
+//     console.log("LEss than 900");
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+

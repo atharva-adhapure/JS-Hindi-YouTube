4d8264a407d63ff4367aac39e2c7fbee0b609a9c
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-04-05 11:51:10 +0000

    truthy and falsy values

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,25 @@
+// const userEmail  = []
+
+// if (userEmail) {
+//     console.log("Got the user Email")
+// }else{
+//     console.log("DOnt have user Email");
+// }
+
+// Falsy values 
+// false , 0 , -0, BigInt 0n , null, undefined, NaN, ""
+
+
+// Truthy values
+// true, 1, "0", "false", " ", [], {}, function(){} 
+
+// if (userEmail.length === 0) {
+//     console.log("Array is Empty");
+    
+// }
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length){
+    console.log("Objext is empty"); 
+}

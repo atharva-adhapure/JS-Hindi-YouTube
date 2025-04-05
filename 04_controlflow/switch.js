cab4d0238e9b6cdbe3739f047d3e21a7ecfca0a1
atharva-adhapure <157288766+atharva-adhapure@users.noreply.github.com>
2025-04-05 14:15:47 +0000

    switch case

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,24 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 3;
+
+switch (month) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("FEb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    default:
+        break;
+}
